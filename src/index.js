a7ec620d5cd6741bfa41491e00e45d9993efac88
Bas Heerschop <basheerschop@gmail.com>
2017-03-21 19:41:23 +0100

    Linting

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import './index.css';
 import numeral from 'numeral';
 
 const price = numeral(1000).format('$0.00');
-console.log(`The price is ${courseValue}, order now!`);
+console.log(`The price is ${price}, order now!`); // eslint-disable-line no-console
