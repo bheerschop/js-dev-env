603f5031759d09be15910d4afe799b9859b64b20
Bas Heerschop <basheerschop@gmail.com>
2017-04-01 12:36:17 +0200

    Production deploy

--- src/api/baseUrl.js
+++ src/api/baseUrl.js
@@ -1,5 +1,5 @@
 export default function getBaseUrl() {
-    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : '/';
+    return getQueryStringParameterByName('useMockApi') ? 'http://localhost:3001/' : 'https://apricot-tart-16700.herokuapp.com/';
 }
 
 function getQueryStringParameterByName(name, url) {
